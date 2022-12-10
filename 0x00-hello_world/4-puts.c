a906d7d8904ecc9c5e171bed591c4f035caabb9d
Dalex702 <atandapaul1234@gmail.com>
2022-12-10 08:03:50 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <studio.h>
 
 /**
- * main - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
 
- */
+*/
 
 int main(void)
 
