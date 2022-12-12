dc55268a7b3444a9763eeac0fa004acefd53ddf5
Dalex702 <atandapaul1234@gmail.com>
2022-12-12 05:02:40 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
 /**
- * main - prints the size of the various types
- * Return: 0 if exited properly, non-zero otherwise
- */
+* main - prints the size of the various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return(0);
+	return (0);
 }
 
 
