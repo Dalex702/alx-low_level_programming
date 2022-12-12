c532bbcc26536b5551e6ec0bd4ae1a1809b47d59
Dalex702 <atandapaul1234@gmail.com>
2022-12-12 13:03:38 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++ )
+	{
+		putchar(x);
+	}
+
+	for (x = 65; x <= 90; x++)
+	{
+		putchar(x);
+	}
+
+	putchar(x);
+	return (0);
