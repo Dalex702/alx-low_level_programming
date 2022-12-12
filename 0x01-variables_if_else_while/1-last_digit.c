96c47f02ed59890550a936fe9eddcf5830350dab
Dalex702 <atandapaul1234@gmail.com>
2022-12-12 09:14:26 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,11 +28,12 @@ int main(void)
 
 	if (y == 0)
 	{
-		printf("and is 0\n");
+		printf("and is 0");
 	} 
+
 	if ( y < 6 && y !=0)
 	{
-		printf("and is less than 6 and not 0\n");
+		printf("and is less than 6 and not 0");
 	}
 
 	printf("\n");
