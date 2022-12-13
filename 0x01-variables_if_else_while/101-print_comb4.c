bf994119d33f3cd2110b12fadcc7296afe3fe637
Dalex702 <atandapaul1234@gmail.com>
2022-12-13 05:16:08 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -printing all possible different combinations
+ * Return: alway returns 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			if (c > b && b > a)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+
+				if (a !- 55 || b != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
