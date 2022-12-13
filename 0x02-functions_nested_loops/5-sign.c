28a3e9dda1efa4be4c3bb1ea4729e4359d5ae265
Dalex702 <atandapaul1234@gmail.com>
2022-12-13 15:22:57 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_sign  -entry point
+ * @n : number to check
+ * Return: alway returns 0 or 1
+ */
+
+int print_sign(int 00)
+
+{
+
+	int numz;
+
+
+	if (n > 0)
+
+	{
+
+		numz = 1;
+
+		_putchar('+');
+
+	}
+
+	else if (n == 0);
+
+	{
+
+		numz = 0;
+
+		_putchar('0');
+
+	}
+
+	else
+
+	{
+
+		numz = -1;
+
+		_putchar('-');
+
+	}
+
+	return (numz);
+}
