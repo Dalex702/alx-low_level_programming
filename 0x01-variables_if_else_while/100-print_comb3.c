0a94aabe401b18d3e49fec329f3af755e08ff57e
Dalex702 <atandapaul1234@gmail.com>
2022-12-13 04:42:10 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- *
- * main - entry point
- *
+ * main - A random number is assigned to the variable j each time it is executed and the last digit of the number stored in the variable j is printed
  * Return: alway returns 0
- *
  */
+
 int main(void)
 {
 	int i;
@@ -18,21 +16,17 @@ int main(void)
 			if (j > i)
 			{
 				putchar(i);
-
 				putchar(j);
 
 				if (i != 56 || j != 57)
 				{
-					putchar(44);
-					
+					putchar(44);				
 					putchar(32);
 				}
 			}
 		}
 	}
-	
 	putchar(10);
-
 	return (0);
 }
 				
