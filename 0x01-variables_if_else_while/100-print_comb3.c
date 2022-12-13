cf6fce1f55c040954d29efeffb3ac42e6a8a015a
Dalex702 <atandapaul1234@gmail.com>
2022-12-13 04:36:57 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ *
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+
+				putchar(j);
+
+				if (i != 56 || j != 57)
+				{
+					putchar(44);
+					
+					putchar(32);
+				}
+			}
+		}
+	}
+	
+	putchar(10);
+
+	return (0);
+}
+				
+
+
+
