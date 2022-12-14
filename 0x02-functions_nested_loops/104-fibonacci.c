ddfe7f65be23a4a20f755938bae3f0cc03ae447b
Dalex702 <atandapaul1234@gmail.com>
2022-12-13 17:46:11 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,7 +14,7 @@ int main(void)
 
 	printf("%.0f, ", a);
 	printf("%.0f, ", b);
-	while (counter < 99)
+	while (counter < 98)
 	{
 
 		counter++;
@@ -22,7 +22,7 @@ int main(void)
 		a = b;
 		b = c;
 		c = a + b;
-		if (counter < 99)
+		if (counter < 98)
 		{
 			printf(", ");
 		}
